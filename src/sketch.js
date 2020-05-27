5c44fecc6c94cf7710466e3e672a3d61be336616
Immodal <kychin.n@gmail.com>
2020-05-28 00:02:19 +1000

    Implemented grid validation

--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,4 @@
-EASY_EXAMPLE = [
-  [6,1,0,0,0,2,0,0,0,],
-  [0,0,0,0,3,6,9,0,0,],
-  [3,5,0,0,9,8,0,1,0,],
-  [7,3,0,0,0,9,4,0,0,],
-  [1,0,0,3,6,4,0,0,2,],
-  [0,0,6,8,0,0,0,3,9,],
-  [0,2,0,9,1,0,0,8,3,],
-  [0,0,3,2,8,0,0,0,0,],
-  [0,0,0,6,0,0,0,9,7,],]
-
+/*
 const sketch = ( p ) => {
   p.setup = () => {
   }
@@ -16,17 +6,63 @@ const sketch = ( p ) => {
   p.draw = () => {
   }
 }
+let p5Instance = new p5(sketch);
+*/
 
 // Grid functions
-const sgrid = {
-  mkEmpty: size => Array.from(Array(size), _ => Array(size).fill(0)),
-  copy: grid => grid.map(row => row.slice()),
-  // For each value in the a row, append "<value>," to its accumulator
-  // For each row, append the outcome of row.reduce and "\n" to its accumulator
-  toString: grid => grid.reduce((ga, row) => ga + row.reduce((ra, v) => ra + "," + v) + "\n", "")
+const fnGrid = {
+  validate: ({ symbols, matrix }) => {
+
+    return colSymbolsAreUnique(matrix)//symbolsAreValid(matrix, symbols) && rowSymbolsAreUnique(matrix)
+  },
+  // For all cells, check if their value can be found in symbols
+  validateSymbols: ({ symbols, matrix }) => matrix.every(row => row.every(v => symbols.indexOf(v)>=0)),
+  // For all rows, check that each value is unique, but not necessarily a valid symbol
+  // For each row, make a copy (because sort sorts in-place), sort the values, and if value is equal to the one before it, return false
+  validateRows: ({ matrix }) => matrix.every(row => row.concat().sort().reduce((ra, v, i, arr) => (i!=0 && ra) ? v!=arr[i-1] : ra, true)),
+  // Horribly inefficient, but convenient. Transpose matrix then what were originally columns are now rows
+  validateCols: ({ matrix }) => validateRows(matrix[0].map((col, i) => matrix.map(row => row[i]))),
+  // No idea how to do this without for-loops
+  validateBlocks: ({ matrix }) => {
+    let blockLength = Math.floor(Math.sqrt(matrix.length))
+    // Iterate through blocks in grid
+    for (let blockRow=0; blockRow<matrix.length; blockRow+=blockLength) {
+      for (let blockCol=0; blockCol<matrix.length; blockCol+=blockLength) {
+        // Check cells in block
+        let values = new Set();
+        for (let i=0; i<blockLength; i++) {
+          for (let j=0; j<blockLength; j++) {
+            const value = matrix[blockRow+i][blockCol+j]
+            if (value==" " || values.has(value)) {
+              // if cell is empty or value has been seen before
+              return false
+            } else {
+              values.add(value)
+            }
+          }
+        }
+      }
+    }
+    return true;
+  },
+  // Make a shallow copy of a given state
+  copy: ({ symbols, matrix }) => {
+    return {
+      symbols: symbols.concat(),
+      matrix: matrix.map(row => row.concat()),
+    }
+  },
+  // Generate a string for exporting the state of the puzzle
+  exportString: ({ symbols, matrix }) => symbols.join(" ") + "\n" + matrix.reduce((ga, row) => ga + row.reduce((ra, v) => ra + "," + v) + "\n", ""),
+  // Generate a grid from a string
+  importString: str => {
+    const data = str.split("\n")
+    return {
+      symbols: data[0].split(" "),
+      matrix: data.slice(1).map(row => row.split(",")),
+    }
+  }
 }
 
-let p5Instance = new p5(sketch);
-
-
-console.log(sgrid.toString(EASY_EXAMPLE))
+const grid = fnGrid.importString(testGamesCompletedInvalid.a)
+fnGrid.validate(grid)
